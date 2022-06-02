7473dcd85182a32316f58ff7da0d9e67bbafbbd7
Karol Koptyra <koptyrakarol@gmail.com>
2022-06-02 14:15:49 +0200

    Update main

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from src.node import Node
 from src.edge import Edge
 
 if __name__ == "__main__":
-    pkl_re = re.compile('.*P[0-9]*\\\\dag.pkl')
-    data_files = [os.path.join(path, name) for path, _, files in os.walk('.\\data') for name in files]
-    graph_files = list(filter(pkl_re.match, data_files)) 
-    print(f"Currently avalible graph files: {graph_files}")
-    gp = GraphParameters(graph_files[1])
+    # pkl_re = re.compile('.*P[0-9]*\\\\dag.pkl')
+    # data_files = [os.path.join(path, name) for path, _, files in os.walk('.\\data') for name in files]
+    # graph_files = list(filter(pkl_re.match, data_files)) 
+    # print(f"Currently avalible graph files: {graph_files}")
+    graph_file = 'data/P07/dag.pkl'
+    reconstruction_file = 'data/P07/reconstruction.npy'
+    gp = GraphParameters(graph_file, reconstruction_file)
     gs = GraphStats(gp.dag)
     gs.generate_all_stats()
     print(gp.dag.root.data)
