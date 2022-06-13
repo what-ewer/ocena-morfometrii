83ee51ec7e8149dd3b64127894b06a112e11d716
Karol Koptyra <koptyrakarol@gmail.com>
2022-06-13 23:21:43 +0200

    Add generational comparison for diameter + change plots

--- src/dag_generational_comparison.py
+++ src/dag_generational_comparison.py
@@ -8,7 +8,7 @@ class DAG_GenerationalComparison:
         assert len(dags) == len(dag_names)
         self.max_gen = max_gen
 
-    def __get_plot_comparison(self, data, title):
+    def __get_plot_comparison(self, data, title, save=True):
         figure, axis = plt.subplots(self.max_gen, 1)
         figure.set_size_inches(8, self.max_gen * 4)
         for gen in range(1, self.max_gen+1):
@@ -17,10 +17,13 @@ class DAG_GenerationalComparison:
             
         plt.xlabel('Liver Graph')
         plt.ylabel('Generation')
-        plt.savefig(f"results/graph_comparison_{title}")
-        plt.clf()
+        if save:
+            plt.savefig(f"results/graph_comparison_{title}")
+            plt.clf()
+        else:
+            plt.show()
 
-    def __get_boxplot_comparison(self, data, title):
+    def __get_boxplot_comparison(self, data, title, save=True):
         figure, axis = plt.subplots(self.max_gen, 1)
         figure.set_size_inches(8, self.max_gen * 4)
         plt.setp(axis, xticklabels=self.dag_names)
@@ -30,31 +33,33 @@ class DAG_GenerationalComparison:
             
         plt.xlabel('Liver Graph')
         plt.ylabel('Generation')
-        plt.savefig(f"results/graph_comparison_{title}")
-        plt.clf()
+        if save:
+            plt.savefig(f"results/graph_comparison_{title}")
+            plt.clf()
+        else:
+            plt.show()
 
-    def __generational_comparison(self, data, title, y_label):
-        for d in data:
-            sd = sum(d)
-            for i in range(len(d)):
-                d[i] /= sd
-
+    def __generational_comparison(self, data, title, y_label, save=True):
         gens = [i+1 for i in range(self.max_gen)]
         plot_data = []
         
         for i,d in enumerate(data):
             plot_data.append(gens)
             plot_data.append(d)
+            plot_data.append(".")
 
         plt.figure(figsize=(8,8))
         plt.plot(*plot_data)
         plt.legend(self.dag_names)
         plt.ylabel(y_label)
         plt.xlabel('Generation')
-        plt.savefig(f"results/generational_comparison_{title}")
-        plt.clf()
+        if save:
+            plt.savefig(f"results/generational_comparison_{title}")
+            plt.clf()
+        else:
+            plt.show()
 
-    def compare_lengths(self):
+    def compare_lengths(self, save=True):
         lengths_per_gen_per_graph = [[[] for _ in range(len(self.dags))] for _ in range(self.max_gen)]
 
         for i, d in enumerate(self.dags):
@@ -65,12 +70,16 @@ class DAG_GenerationalComparison:
 
         edges_per_gen_per_graph = [[len(lengths_per_gen_per_graph[g][i]) for i in range(len(self.dags))] for g in range(self.max_gen)]
         edges_per_graph_per_gen = [[edges_per_gen_per_graph[g][i] for g in range(self.max_gen)] for i in range(len(self.dags))]
+        for d in edges_per_graph_per_gen:
+            sd = sum(d)
+            for i in range(len(d)):
+                d[i] /= sd
 
-        self.__get_boxplot_comparison(lengths_per_gen_per_graph, "lengths_per_generation")
-        self.__get_plot_comparison(edges_per_gen_per_graph, "edges_per_generation")
-        self.__generational_comparison(edges_per_graph_per_gen, "edges_per_graph_per_gen", "edges / total edges")
+        self.__get_boxplot_comparison(lengths_per_gen_per_graph, "lengths_per_generation", save)
+        self.__get_plot_comparison(edges_per_gen_per_graph, "edges_per_generation", save)
+        self.__generational_comparison(edges_per_graph_per_gen, "edges_per_graph_per_gen", "edges / total edges", save)
         
-    def compare_diameters(self):
+    def compare_diameters(self, save=True):
         diameters_per_gen_per_graph = [[[] for _ in range(len(self.dags))] for _ in range(self.max_gen)]
         for i, d in enumerate(self.dags):
             diameters_generations = [(edge['mean_radius'], edge['generation']) for edge in d.edges]
@@ -78,9 +87,12 @@ class DAG_GenerationalComparison:
                 if (e[1]-1 < self.max_gen):
                     diameters_per_gen_per_graph[e[1]-1][i].append(e[0])
 
-        self.__get_boxplot_comparison(diameters_per_gen_per_graph, "diameters_per_generation")
+        diameters_per_graph_per_gen = [[np.average(diameters_per_gen_per_graph[g][i]) for g in range(self.max_gen)] for i in range(len(self.dags))]
 
-    def compare_bifurcation_angles(self):
+        self.__get_boxplot_comparison(diameters_per_gen_per_graph, "diameters_per_generation", save)
+        self.__generational_comparison(diameters_per_graph_per_gen, "avg_diameters_per_graph_per_gen", "mean diameter", save)
+
+    def compare_bifurcation_angles(self, save=True):
         bifurcation_angles_per_gen_per_graph = [[[] for _ in range(len(self.dags))] for _ in range(self.max_gen)]
         for i, d in enumerate(self.dags):
             angles_generations = [(edge['relative_angle'] / np.pi * 180, edge['generation']) for edge in d.edges[1:]]
@@ -88,9 +100,9 @@ class DAG_GenerationalComparison:
                 if (e[1]-1 < self.max_gen):
                     bifurcation_angles_per_gen_per_graph[e[1]-1][i].append(e[0])
 
-        self.__get_boxplot_comparison(bifurcation_angles_per_gen_per_graph, "bifurcation_angles_per_generation")
+        self.__get_boxplot_comparison(bifurcation_angles_per_gen_per_graph, "bifurcation_angles_per_generation", save)
 
-    def compare_tortuosities(self):
+    def compare_tortuosities(self, save=True):
         tortuosities_per_gen_per_graph = [[[] for _ in range(len(self.dags))] for _ in range(self.max_gen)]
         for i, d in enumerate(self.dags):
             tortuosities_generations = [(edge['tortuosity'], edge['generation']) for edge in d.edges]
@@ -98,9 +110,9 @@ class DAG_GenerationalComparison:
                 if (e[1]-1 < self.max_gen):
                     tortuosities_per_gen_per_graph[e[1]-1][i].append(e[0])
 
-        self.__get_boxplot_comparison(tortuosities_per_gen_per_graph, "tortuosities_per_generation")
+        self.__get_boxplot_comparison(tortuosities_per_gen_per_graph, "tortuosities_per_generation", save)
 
-    def compare_interstital_distances(self):
+    def compare_interstital_distances(self, save=True):
         interstitial_distances_per_gen_per_graph = [[[] for _ in range(len(self.dags))] for _ in range(self.max_gen)]
         for i, d in enumerate(self.dags):
             interstitial_distances_generations = [(edge['interstitial_distance'], edge['generation']) for edge in d.edges]
@@ -108,7 +120,7 @@ class DAG_GenerationalComparison:
                 if (e[1]-1 < self.max_gen):
                     interstitial_distances_per_gen_per_graph[e[1]-1][i].append(e[0])
 
-        self.__get_boxplot_comparison(interstitial_distances_per_gen_per_graph, "interstitial_distances_angles_per_generation")
+        self.__get_boxplot_comparison(interstitial_distances_per_gen_per_graph, "interstitial_distances_angles_per_generation", save)
 
     def compare_all(self):
         self.compare_lengths()
