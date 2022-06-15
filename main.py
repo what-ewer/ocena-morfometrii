4f35fa7d5c9995f4032ce6d0ebff1a053d4cd59f
Karol Koptyra <koptyrakarol@gmail.com>
2022-06-15 12:15:24 +0200

    Change main.py and ipynb

--- main.py
+++ main.py
@@ -5,24 +5,21 @@ from src.dag_generational_comparison import DAG_GenerationalComparison
 from src.dag import DAG
 from src.node import Node
 from src.edge import Edge
+from src.utils import load_dag
 
 if __name__ == "__main__":
-    pkl_re = re.compile('.*P[0-9]*\\\\dag.pkl')
+    pkl_stats_re = re.compile('.*P[0-9]*\\\\dag_with_stats.pkl')
     data_files = [os.path.join(path, name) for path, _, files in os.walk('.\\data') for name in files]
-    graph_files = list(filter(pkl_re.match, data_files)) 
-    print(f"Currently avalible graph files: {graph_files}")
+    graph_files = list(filter(pkl_stats_re.match, data_files)) 
+    print(f"Currently avalible graph with stats files: {graph_files}")
 
     dag_names = []
     dags = []
-
     for g in graph_files:
         dag_id = g.split('data\\')[1].split('\\')[0]
-        rec_file = g.split('dag.pkl')[0] + 'reconstruction.npy'
-        gp = GraphParameters(g, rec_file, dag_id=dag_id)
-        gs = GraphStats(gp.dag, dag_id)
-        gs.generate_all_stats()
+        dag = load_dag(g)
 
-        dags.append(gp.dag)
+        dags.append(dag)
         dag_names.append(dag_id)
 
     dag_gen_comparison = DAG_GenerationalComparison(dags, dag_names, 8)
